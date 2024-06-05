e7e07782ff0ac9a6506a392a7a6cd34b5139253f
IrinaOleynik <irinaoleynik615@gmail.com>
2024-06-05 17:05:29 +0300

    feature/handlers created

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,96 +1,152 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.*;
+
+
 
 public class Server {
-    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    //private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final int THREADS = 64;
     final ExecutorService threadPool;
+    private final Map<String, Map<String, Handler>> handlers;
+    final List<String> allowedMethods = List.of("GET", "POST");
 
     public Server() {
         this.threadPool = Executors.newFixedThreadPool(THREADS);
+        this.handlers = new ConcurrentHashMap<>();
     }
 
-    public void start(int port) throws IOException {
+    public void start(int port) {
         try (final ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 final Socket socket = serverSocket.accept();
-                threadPool.submit(() -> request(socket));
+                threadPool.submit(() -> requestProcess(socket));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void request(Socket socket){
-        try (final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            final String requestLine = in.readLine();
-            final String[] parts = requestLine.split(" ");
-
-            if (parts.length != 3) {
-                socket.close();
-            } else {
-                response(socket,parts);
+    private void requestProcess(Socket socket) {
+        try {
+            final Request request = parse(socket);
+            if (!handlers.containsKey(request.getMethod())) {
+                notFound(socket);
             }
-
+            if (!handlers.get(request.getMethod()).containsKey(request.getPath())) {
+                notFound(socket);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void response(Socket socket, String[] parts){
-        try (final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())){
-            final String path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
-            }
-            final Path filePath = Path.of(".", "public", path);
-            final String mimeType = Files.probeContentType(filePath);
-
-            if (path.equals("/classic.html")) {
-                final String template = Files.readString(filePath);
-                final byte[] content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
-                out.flush();
-            }
-
-            final long length = Files.size(filePath);
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, out);
-            out.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void addHandler(String method, String path, Handler handler){
+        if (!handlers.containsKey(method)) {
+            handlers.put(method, new ConcurrentHashMap<>());
         }
+        handlers.get(method).put(path, handler);
+    }
+
+    private Request parse(Socket socket) throws IOException {
+        final BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+        final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+
+        final int limit = 4096;
+        in.mark(limit);
+        final byte[] buffer = new byte[limit];
+        final int read = in.read(buffer);
+
+        final byte[] requestLineDelimiter = new byte[]{'\r', '\n'};
+        final int requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+            socket.close();
+        }
+        final String[] requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+            socket.close();
+        }
+        final String method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            badRequest(out);
+            socket.close();
+        }
+        final String path = requestLine[1];
+        if (!path.startsWith("/")) {
+            badRequest(out);
+            socket.close();
+        }
+        final byte[] headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final int headersStart = requestLineEnd + requestLineDelimiter.length;
+        final int headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+            socket.close();
+        }
+        in.reset();
+        in.skip(headersStart);
+
+        final byte[] headersBytes = in.readNBytes(headersEnd - headersStart);
+        final List<String> headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+        String body = "";
+        if (!method.equals("GET")) {
+            in.skip(headersDelimiter.length);
+            final Optional<String> contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                final int length = Integer.parseInt(contentLength.get());
+                final byte[] bodyBytes = in.readNBytes(length);
+                body = new String(bodyBytes);
+            }
+        }
+        return new Request(method, path, headers, body);
+    }
+    private int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    private void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    private Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+    private void notFound(Socket socket) throws IOException {
+        final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
     }
 }
